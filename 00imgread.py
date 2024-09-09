4c4b2606dc0424cf6c9233e444ad384d8e05e114
108-0 <yj021225@g.shingu.ac.kr>
2024-09-09 14:15:00 +0900

    1st commit

---
+++ 00imgread.py
@@ -0,0 +1,31 @@
+import cv2
+img_file = "./img/girl.jpg"
+img = cv2.imread(img_file)
+cv2.imshow("IMG", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+from PIL import Image
+img = Image.open("./img/girl.jpg")
+img.show()
+
+# 그레이 스케일로 읽기
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+cv2.imshow("IMG", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+video_file = "./img/big_buck.avi"
+cap = cv2.VideoCapture(video_file)
+if cap.isOpened():
+    while True:
+        ret, img = cap.read()
+        if ret:
+            cv2.imshow(video_file, img)
+            cv2.waitKey(25)
+        else:
+            break
+else:
+    print("cannot open video")
+cap.release()
+cv2.destroyAllWindows()
