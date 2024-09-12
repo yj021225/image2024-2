12de5a36ebb2327771911bb332bdd5317999a04b
108-0 <yj021225@g.shingu.ac.kr>
2024-09-12 16:02:13 +0900

    1st commit

--- 04letters.py
+++ 04letters.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
 img = np.full((500, 500, 3), 255, dtype=np.uint8)
 
 # sans-serif small
@@ -20,9 +21,21 @@ cv2.putText(img, "Complex", (50, 260), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 0))
 # serif normal X 2
 cv2.putText(img, "Complex", (200, 260), cv2.FONT_HERSHEY_TRIPLEX, 2, (0, 0, 255))
 
-# 한글
-cv2.putText(img, "아름다운 강산 - 전영준", (50, 470), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0))
+# OpenCV 이미지를 PIL 이미지로 변환
+img_pil = Image.fromarray(img)
+
+# 드로우 객체 생성
+draw = ImageDraw.Draw(img_pil)
+
+# 사용할 폰트 설정 (예: 나눔고딕, 크기 40)
+font = ImageFont.truetype("./fonts/NanumGothic.ttf", 20)
+
+# 텍스트를 이미지에 그림 (한글 포함)
+draw.text((50, 470), "아름다운 강산 - 전영준", font=font, fill=(0, 0, 0))
+
+# PIL 이미지를 다시 OpenCV 이미지로 변환
+img = np.array(img_pil)
 
 cv2.imshow('draw text', img)
-cv2.waitKey()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ 07ColorSpaceConversion.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+img_file = "./img/girl.jpg"
+img = cv2.imread(img_file)
+print(img.shape)
+
+# cv2.imshow("IMG", img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+imgy = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+y, u, v = cv2.split(imgy)
+
+cv2.imshow("IMG2", y)
+cv2.imshow("IMG", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+imgyy = np.full((293, 406), 255, dtype=np.uint8)
+b, g, r = cv2.split(img)
+imgyy = (b * 0.114) + (g * 0.587) + (r * 0.299)
+print(imgyy)
+imgyy = imgyy.astype(np.uint8)
+diff = y - imgyy
+print(diff)
+cv2.imshow("IMG", img)
+cv2.imshow("IMG cv2 변환", y)
+cv2.imshow("IMG 계산변환", imgyy)
+cv2.imshow("diff", diff)
+cv2.waitKey()
+cv2.destroyAllWindows()
